be9bbe3065a9d96f91b9ee9edf8f605f749016f6
Suha523 <suhashehadeh123@gmail.com>
2022-01-04 16:39:06 +0200

    landing-website-laravel-project

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+//use Mcamara\LaravelLocalization\LaravelLocalization;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => ['auth', 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
+    ], function(){
+        Route::get('/dashboard', function () {
+            return view('layouts.backend.index');
+        })->name('dashboard');
+    });
+
+
+// Route::get('/', function () {
+//     return view('layouts.backend.index');
+// });
+
+
+
+require __DIR__.'/auth.php';
